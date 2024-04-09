450983f58f9dd7376f04baa1d3b52c04ca056ab1
Tony Kay <tony.kay@gmail.com>
2024-04-09 16:48:47 -0700

    1.6 exercises

--- exercises/section1_6.clj
+++ exercises/section1_6.clj
@@ -1,5 +1,6 @@
 (ns section1_6
-  "Making Things Happen.")
+  "Making Things Happen."
+  (:require [clojure.string :as str]))
 
 ;; EXERCISE: Look up the formula for converting from Fahrenheit to Celcius. Write a function that takes F as input, and outputs the temp in C.
 ;; be sure to test it on some values that you know the answer for, like boiling point and freezing. Name the function `f-to-c`
@@ -49,6 +50,11 @@
 ;; (perhaps with some slight rounding errors)
 
 
+;; EXERCISE: Write a function called `with-full-name`. It should accept a single argument called `person`, which is a map
+;; where you expect there to be keys for `:person/first-name` and `:person/last-name`. Return the same `person` as you
+;; receive, but use `assoc` and `str` to add a `:person/full-name` key with a value that is what you would expect.
+
+
 ;; EXERCISE: In the samples below: Finish the code so that the output is as shown:
 
 (map (fn [card]) [{:card/rank 3} {:card/rank 10}])
@@ -86,7 +92,7 @@
 ;; Thus, it makes total sense to treat the (the data structure) as a way to calculate results for a finite number of
 ;; questions. In other words instead of `f(x)` being math calculations, `f(x)` could just be a straight "dictionary lookup"
 
-;; If you did all of the bonus things in the book, then you've already seen that maps can act like functions in Clojure.
+;; If you did the BONUS things in the book, then you've already seen that maps (the data structure) can act like functions in Clojure.
 ;; they return the known value for that key, or nil if there is no such entry. For example, try running these:
 
 (map {1  2
@@ -105,4 +111,103 @@
 
 ;; EXERCISE: Change `map` to `keep` in the examples above. Do you understand the results? Which of those seems
 ;; the most "useful", if any? Why?
+
+
+;; There are two other utility functions in Clojure that can come in handy: `constantly` and `identity`.
+;; The first takes a single value, and returns a function that always returns that value:
+
+(def yes-function (constantly true))
+(yes-function "hello")
+(yes-function false)
+(yes-function 42)
+
+;; The other one is a function that just returns whatever it is passed:
+(identity 42)
+(identity "Hello")
+(identity 8.9)
+
+;; These come in handy when you are building more complex things, where functions can be "plugged in" to accomplish
+;; some result. For example, let's say I gave you this function:
+
+(defn calculate [custom-filter]
+  (filter custom-filter (map inc [1 2 3 4 5])))
+
+;; But you decided you just want everything the is calculated
+(calculate (constantly true))
+
+;; or a more complex function that let's you plug in post-processing:
+(defn calculate2 [custom-filter post-process]
+  (map post-process
+    (filter custom-filter
+      (map inc [1 2 3 4 5]))))
+
+;; EXERCISE: What will the output of this be (guess before running):
+(calculate2 odd? inc)
+
+;; EXERCISE: What will the result of this be?
+(calculate2 (constantly true) identity)
+
+
+;; There is a built-in thing called Math which has a number of useful math functions. For example, to get the
+;; square root of a number you would write:
+(Math/sqrt 6)
+
+;; and to round a number:
+(Math/round 6.2)
+(Math/round 6.8)
+
+;; EXERCISE: Write a Clojure function that find the length of a hypotenuse of a right triangle: hypot(x, y) = sqroot ( x * x + y * y )
+
+
+;; The following function will calculate the x coordinate (really we'd have +x and -x) for a circle, given a y coordinate
+(defn circle-x [radius y]
+  (when (<= (- radius) y radius)
+    (let [x (Math/sqrt
+              (-
+                (* radius radius)
+                (* y y)))]
+      x)))
+
+;; EXERCISE: Use `map` to get the x coordinates for a circle of radius 3, using the y values [-3 -2 -1 0 1 2 3]
+
+
+;; The `doseq` function binds names to sequences (one element at a time). This is similar to map, but instead of
+;; it taking a function and producing a new sequence, it expects you to do some "side effect" against the
+;; real world with those values. For example, the `println` function prints a string to the "output":
+(doseq [name ["Bob" "Tash" "Tony"]]
+  (println "Hello" name))
+
+;; We can use this along with our "circle calculator" to make a rough on-screen plot of a circle!
+;; We'll do this by "shifting" the points of the circle to the right so that we can just use spaces and * to draw it.
+;; We'll also "round" the numbers, since the columns of our screen are integers (0 1 2 3, etc).
+(doseq [y [-3 -2 -1 0 1 2 3]]
+  (let [radius       3
+        x            (Math/round (circle-x radius y))       ; plus x
+        neg-x        (- x)                                  ; The minus of x
+        x-offset     10                                     ; Some space to shift the "plot" to the right
+        ;; The `repeat` function returns a sequence of the same thing over and over...we'll use that to make
+        ;; sequences of spaces. The `str/join` will join sequences of strings together as a single string
+        ;; (str is an alias for clojure.string. See the require at the top of the file)
+        left-padding (str/join (repeat (+ x-offset neg-x) " "))
+        ;; How wide is the circle. Well, it is centered on the y axis, so we have -x to x, or 2x
+        width        (* 2 x)
+        ;; Make enough spaces to go between -x and x
+        width-spaces (str/join (repeat width " "))]
+    ;; Combine the bits together into an on-screen "line". We're leveraging the fact that the terminal
+    ;; prints one line after enough, which is why we started at the "bottom" of the circle. Technically,
+    ;; we're drawing it "upside down"...but it's a circle so who cares?
+    (println (str left-padding "*" width-spaces "*"))))
+
+;; EXERCISE: Try out the following:
+(range -3 3)
+(range -3 4)
+(range -6 7)
+
+;; EXERCISE: Change the code in the doseq above to use `range` instead of the literal [-3 -2 -1 0 1 2 3]
+
+;; EXERCISE: Wrap the above `doseq` in a function called `draw-circle`, which takes TWO parameters:
+;; the `x-offset`, and the desired `radius`. Remember to fix your range!
+;; Test out your new `draw-circle` function to see that you can draw various
+;; sized circles!
+
 
